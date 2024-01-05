cc449f8b1b45749e0efe8fc9c84e87d3c0bd8981
dorian30 <alvaradogdorian@gmail.com>
2024-01-05 18:24:00 +0100

    chore: add diagonal 3D perspective

--- lessons/03-basic-scene/script.js
+++ lessons/03-basic-scene/script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 // Canvas
 const canvas = document.getElementById("webgl");
@@ -15,7 +15,7 @@ scene.add(mesh);
 // Sizes
 const sizes = {
   width: 800,
-  height: 600
+  height: 600,
 };
 
 // Camera
@@ -23,8 +23,18 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
+// Axes helper
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+
+camera.position.z = 1.5;
+camera.position.x = 1.5;
+camera.position.y = 1.5;
+camera.rotation.reorder("YXZ");
+camera.rotation.x = -Math.PI / 4;
+camera.rotation.y = Math.PI / 4;
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
-
